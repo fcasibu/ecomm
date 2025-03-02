465695bf5f5b2b34998abbc32e86f7000755f97c
Francis Eugene Casibu <projectnevz@gmail.com>
2025-03-02 22:19:26 +0800

    improve usability of search

--- apps/cms/src/features/categories/components/category-search.tsx
+++ apps/cms/src/features/categories/components/category-search.tsx
@@ -4,17 +4,19 @@ import { Button } from "@ecomm/ui/button";
 import { Loader, X } from "lucide-react";
 import { useQueryState } from "nuqs";
 import { Input } from "@ecomm/ui/input";
-import { useTransition } from "react";
+import { useRef, useTransition } from "react";
 
 export function CategorySearch() {
   const [query, setQuery] = useQueryState("q", {
     defaultValue: "",
   });
+
   const [_, setPage] = useQueryState("page", {
     defaultValue: "1",
     shallow: false,
   });
 
+  const hasSearched = useRef(Boolean(query));
   const [isPending, startTransition] = useTransition();
 
   return (
@@ -24,6 +26,7 @@ export function CategorySearch() {
         e.preventDefault();
         startTransition(async () => {
           await setPage("1");
+          hasSearched.current = true;
         });
       }}
     >
@@ -31,7 +34,16 @@ export function CategorySearch() {
         <Input
           type="text"
           value={query || ""}
-          onChange={(e) => setQuery(e.target.value)}
+          onChange={(e) => {
+            setQuery(e.target.value);
+
+            if (hasSearched.current && !e.target.value.length) {
+              startTransition(async () => {
+                await setPage("1");
+                hasSearched.current = false;
+              });
+            }
+          }}
           className="pr-12 py-2 focus:ring-2 focus:ring-primary"
         />
 
@@ -39,18 +51,30 @@ export function CategorySearch() {
           type="button"
           variant="ghost"
           size="sm"
+          disabled={!query}
           className="absolute right-2 top-1/2 -translate-y-1/2 p-1"
           onClick={() => {
+            if (!hasSearched.current) {
+              setQuery("");
+
+              return;
+            }
+
             startTransition(async () => {
               await setQuery("");
               await setPage("1");
+              hasSearched.current = false;
             });
           }}
         >
           <X className="h-4 w-4 text-muted-foreground" />
         </Button>
       </div>
-      <Button type="submit" disabled={isPending} className="min-w-[120px]">
+      <Button
+        type="submit"
+        disabled={isPending || !query}
+        className="min-w-[120px]"
+      >
         {isPending ? <Loader className="animate-spin" size={16} /> : "Search"}
       </Button>
     </form>
