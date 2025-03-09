cfbafa5fb910171d24445fcbef954668844842e6
Francis Eugene Casibu <projectnevz@gmail.com>
2025-03-10 03:34:13 +0800

    center spinner

--- apps/cms/src/app/categories/[id]/page.tsx
+++ apps/cms/src/app/categories/[id]/page.tsx
@@ -8,7 +8,7 @@ export default function Page({ params }: { params: Promise<{ id: string }> }) {
   return (
     <Suspense
       fallback={
-        <div className="max-w-4xl mx-auto p-8 space-y-8">
+        <div className="max-w-4xl mx-auto p-8 space-y-8 flex justify-center items-center h-full">
           <Loader className="animate-spin" />
         </div>
       }
