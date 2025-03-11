02bda00602f6a982ae08dcb80e668617751fc536
Francis Eugene Casibu <projectnevz@gmail.com>
2025-03-12 05:20:31 +0800

    add bundle analyzer

--- apps/cms/next.config.js
+++ apps/cms/next.config.js
@@ -1,5 +1,6 @@
 import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 import { fileURLToPath } from "node:url";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 import createJiti from "jiti";
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
@@ -41,4 +42,7 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+// eslint-disable-next-line turbo/no-undeclared-env-vars, no-undef
+export default withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" })(
+  nextConfig,
+);
