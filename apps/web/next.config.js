9407632e4a915f59311b71e0ffa0c416f3c15868
Francis Eugene Casibu <projectnevz@gmail.com>
2025-03-14 18:37:58 +0800

    add sentry

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,5 @@
 import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+import { withSentryConfig } from "@sentry/nextjs";
 import { fileURLToPath } from "node:url";
 import withBundleAnalyzer from "@next/bundle-analyzer";
 import createJiti from "jiti";
@@ -42,7 +43,35 @@ const nextConfig = {
   },
 };
 
-// eslint-disable-next-line turbo/no-undeclared-env-vars, no-undef
-export default withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" })(
-  nextConfig,
+export default withSentryConfig(
+  // eslint-disable-next-line turbo/no-undeclared-env-vars, no-undef
+  withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" })(nextConfig),
+  {
+    // For all available options, see:
+    // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
+    org: "fcasibu",
+    project: "javascript-nextjs",
+
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your hosting bill.
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
+    // tunnelRoute: "/monitoring",
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+  },
 );
---
+++ apps/web/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://624d220c7d1f7b29a7068d93d8d1624b@o4508974256816128.ingest.us.sentry.io/4508975589883904",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
