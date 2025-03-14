53a9a3c391cf439f46d157fdb99f0153c4ba1a46
Francis Eugene Casibu <projectnevz@gmail.com>
2025-03-14 20:44:53 +0800

    format files

--- apps/test/src/index.ts
+++ apps/test/src/index.ts
@@ -1,8 +1,6 @@
-import { productsController } from "@ecomm/services/registry";
 import { md5 } from "utility";
 
 async function run() {
-  productsController.create({});
   console.log(md5("what is this"));
 }
 
