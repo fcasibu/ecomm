df970127489f3fdbc8ce7568f7b51f5274348572
Francis Eugene Casibu <projectnevz@gmail.com>
2025-03-17 13:19:39 +0800

    remove unused envs

--- apps/web/src/env/server.ts
+++ apps/web/src/env/server.ts
@@ -5,9 +5,6 @@ export const serverEnv = createEnv({
   server: {
     DATABASE_URL: z.string(),
     SENTRY_AUTH_TOKEN: z.string(),
-    SANITY_PROJECT_ID: z.string(),
-    SANITY_DATASET: z.string(),
-    SANITY_API_VERSION: z.string(),
     SANITY_TOKEN: z.string(),
   },
   experimental__runtimeEnv: process.env,
