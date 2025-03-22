099bae85912d34cdc5fa312314031d6249202ef4
Francis Eugene Casibu <projectnevz@gmail.com>
2025-03-22 20:39:52 +0800

    make recently viewed products max 14

--- apps/web/src/features/products/components/recently-viewed-setter.tsx
+++ apps/web/src/features/products/components/recently-viewed-setter.tsx
@@ -1,33 +1,32 @@
 'use client';
 
-import { MAX_RECENTLY_VIEWED_PRODUCTS_DAYS } from '@/lib/constants';
 import { storageKeys } from '@/lib/constants/storage-keys';
 import { storage } from '@/lib/utils/storage';
 import { useCurrentLocale } from '@/locales/client';
 import { useEffect } from 'react';
+
+const EXPIRE_TIME_IN_MS = 10 * 24 * 60 * 60 * 1000;
+const MAX_RECENTLY_VIEWED_PRODUCTS = 14;
 
 export function RecentlyViewedSetter({ sku }: { sku: string }) {
   const locale = useCurrentLocale();
 
   useEffect(() => {
     const now = new Date();
-    const skus = storage.get(storageKeys.recentlyViewedProducts(locale));
+    const skus = storage.get(storageKeys.recentlyViewedProducts(locale)) ?? [];
 
-    const filteredSkus =
-      skus?.filter((visitedSku) => {
-        const expireTime =
-          MAX_RECENTLY_VIEWED_PRODUCTS_DAYS * 24 * 60 * 60 * 1000;
+    const skuFilter = (visitedSku: { value: string; timestamp: number }) =>
+      visitedSku.timestamp - now.getTime() < EXPIRE_TIME_IN_MS &&
+      visitedSku.value !== sku;
 
-        return (
-          visitedSku.timestamp - now.getTime() < expireTime &&
-          visitedSku.value !== sku
-        );
-      }) ?? [];
+    const filteredSkus = skus
+      .filter(skuFilter)
+      .sort((a, b) => a.timestamp - b.timestamp)
+      .slice(0, MAX_RECENTLY_VIEWED_PRODUCTS);
 
-    storage.set(storageKeys.recentlyViewedProducts(locale), [
-      { value: sku, timestamp: now.getTime() },
-      ...filteredSkus,
-    ]);
+    filteredSkus.unshift({ value: sku, timestamp: now.getTime() });
+
+    storage.set(storageKeys.recentlyViewedProducts(locale), filteredSkus);
   }, [locale, sku]);
 
   return null;
